cf46fa82ae74d4e2155dcdc1399a59b1e86be5a2
sosunnyproject <parksosun1103@gmail.com>
2019-05-20 08:50:34 +0900

    fft edit

--- codingTrain17_FFT/sketch.js
+++ codingTrain17_FFT/sketch.js
@@ -20,7 +20,8 @@ function setup() {
   button = createButton('toggle');
   button.mousePressed(toggleSong);
   song.play();
-  fft = new p5.FFT();
+  fft = new p5.FFT(0.8, 512);
+  w = width / 64;
 }
 
 function draw() {
@@ -29,4 +30,12 @@ function draw() {
   console.log(spectrum.length); //default: 1024 frequency length
   stroke(255);
   noFill();
+  for (var i = 0; i < spectrum.length; i++) {
+    var amp = spectrum[i];
+    var y = map(amp, 0, 512, height, 0);
+    // line(i, height, i , y);
+    // rect(i*w, y, i*w, height - y);
+    rect(i*w, y, w, height - y);
+
+  }
 }
