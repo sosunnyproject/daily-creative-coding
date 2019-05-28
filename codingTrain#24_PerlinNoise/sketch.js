153137b33fb3d22f9a6b84c826daac156cfadb1a
sosunnyproject <parksosun1103@gmail.com>
2019-05-28 09:12:13 +0900

    bunch of random vectors instaed of rect

--- codingTrain#24_PerlinNoise/sketch.js
+++ codingTrain#24_PerlinNoise/sketch.js
@@ -12,15 +12,22 @@ function setup(){
 }
 
 function draw() {
+  background(0);
   var yoff = 0;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
       var index = (x + y * width) * 4;
       var r = noise(xoff, yoff) * 255;
+      var v = p5.Vector.fromAngle(random(TWO_PI));  // "i want vector on every spot on the grid."05:10
       xoff += inc;
-      fill(r);
-      rect(x*scl, y*scl, scl, scl);
+      stroke(255);
+      push();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      line(0, 0, scl, 0); // for every vector, rotate according to angle of that random vector
+      pop();
+
     }
     yoff += inc;
   }
