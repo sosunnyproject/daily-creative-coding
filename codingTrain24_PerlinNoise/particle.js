a4559e5f75b82643f6613e7552ccb36761a096d3
sosunnyproject <parksosun1103@gmail.com>
2019-05-31 09:19:25 +0900

    perlin_noise

--- codingTrain24_PerlinNoise/particle.js
+++ codingTrain24_PerlinNoise/particle.js
@@ -2,10 +2,14 @@ function Particle() {
   this.pos = createVector(random(width), random(height));
   this.vel = createVector(0, 0);
   this.acc = createVector(0, 0);
+  this.maxspeed = 4;
+
+  this.prevPos = this.pos.copy();
 
   // update vel based on acc
   this.update = function() {
     this.vel.add(this.acc);
+    this.vel.limit(this.maxspeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
@@ -16,17 +20,36 @@ function Particle() {
     this.acc.add(force);
   }
   this.show = function() {
-    stroke(255);
-    strokeWeight(2);
-    point(this.pos.x, this.pos.y);
+    stroke(255, 5);
+    strokeWeight(1);
+    // point(this.pos.x, this.pos.y);
+    line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y); //draw line from current to previous pos
+    this.updatePrev();
+  }
+
+  this.updatePrev = function() {
+    this.prevPos.x = this.pos.x;
+    this.prevPos.y = this.pos.y;
   }
 
   // make sure particles come back when hit the walls
   this.edges = function() {
-    if(this.pos.x > width) this.pos.x = 0;
-    if(this.pos.x < 0) this.pos.x = width;
-    if(this.pos.y > height) this.pos.y = 0;
-    if(this.pos.y < 0) this.pos.y = height;
+    if(this.pos.x > width) {
+      this.pos.x = 0;
+      this.updatePrev();
+    }
+    if(this.pos.x < 0) {
+      this.pos.x = width;
+      this.updatePrev();
+    }
+    if(this.pos.y > height) {
+      this.pos.y = 0;
+      this.updatePrev();
+    }
+    if(this.pos.y < 0) {
+      this.pos.y = height;
+      this.updatePrev();
+    }
   }
 
   // im a particle, size me up to the scale of one grid square
--- codingTrain24_PerlinNoise/sketch.js
+++ codingTrain24_PerlinNoise/sketch.js
@@ -7,45 +7,50 @@ var particles = [];
 var flowfield;
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(600, 600);
   cols = floor(width/scl);
   rows = floor(height/scl);
   fr = createP(''); // creating Paragraph element
 
   flowfield = new Array(cols*rows); //preset the size of array
 
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 1000; i++) {
     particles[i] = new Particle();
   }
+
+  background(0);
+
 }
 
 function draw() {
-  background(0);
+  // background(0);
+
   var yoff = 0;
   for (var y = 0; y < rows; y++) {
     var xoff = 0;
     for (var x = 0; x < cols; x++) {
       var index = (x + y * cols);
-      var angle = noise(xoff, yoff, zoff) * TWO_PI;
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 3;
       var v = p5.Vector.fromAngle(angle);  // "i want vector on every spot on the grid."05:10
+      v.setMag(2); // full units, no limit..need to set maximum limit
       flowfield[index] = v;
       xoff += inc;
 
-      stroke(255, 100);
-      strokeWeight(0.5);
-      push();
-      translate(x * scl, y * scl);
-      rotate(v.heading());
-      line(0, 0, scl, 0); // for every vector, rotate according to angle of that random vector
-      pop();
+      // stroke(255, 100);
+      // strokeWeight(0.5);
+      // push();
+      // translate(x * scl, y * scl);
+      // rotate(v.heading());
+      // line(0, 0, scl, 0); // for every vector, rotate according to angle of that random vector
+      // pop();
 
     }
     yoff += inc;
-    //zoff += 0.001;  // fixed flow field, if you comment this
+    zoff += 0.00004;  // fixed flow field, if you comment this
   }
   for (var i = 0; i < particles.length; i++) {
     particles[i].follow(flowfield);
-    // find appropriate, nearby vector 
+    // find appropriate, nearby vector
     particles[i].update();
     particles[i].show();
     particles[i].edges();
