75fc4168e171c56c1d0d5ddcb68d78f18a26dbb3
sosunnyproject <parksosun1103@gmail.com>
2019-08-31 13:07:09 +0900

    edit for slider design

--- codingChallenge55_MathRose/sketch_slider.js
+++ codingChallenge55_MathRose/sketch_slider.js
@@ -1,10 +1,9 @@
 let d = 9;
 let n = 4;
-let locX, locY;
 let hue1, hue2, strokeW, count, newX, newY, angle, diff;
 let sliderD, sliderN, sliderH, sliderW, sliderC, sliderX, sliderY, sliderA;
-let r = 10;
-function setup() {
+
+ function setup() {
   createCanvas(750, 450);
   sliderD = createSlider(1, 10, 5, 0.2);
   sliderN = createSlider(5, 15, 5, 0.2);
@@ -12,28 +11,28 @@ function setup() {
   sliderW =  createSlider(0.5, 5, 1, 0.5);
   sliderC = createSlider(1, 10, 3, 1);
 
-  sliderX = createSlider(0, 100, 0, 1)
+   sliderX = createSlider(0, 200, 0, 1)
   sliderY = createSlider(0, 200, 0, 1)
 
-  sliderA = createSlider(0.01, 3, 0.5, 0.01);
+   sliderA = createSlider(0.01, 3, 0.5, 0.01);
   colorMode(HSB);
 
-}
+ }
 
-function draw() {
+ function draw() {
   background(hue1, 100, 100);
 
-  d = sliderD.value();
+   d = sliderD.value();
   n = sliderN.value();
   hue1 = sliderH.value();
   hue2 = (hue1+180)%240;
   strokeW = sliderW.value();
   count = sliderC.value();
-  gapI = sliderX.value();
+  newX = sliderX.value();
   newY = sliderY.value();
   angle = sliderA.value();
 
-  textSize(12);
+   textSize(12);
   fill(200);
   text('d:', 10, 30);
   text(d, 20, 30);
@@ -41,40 +40,35 @@ function draw() {
   text(n, 55, 30);
   text('count:', 0, 50);
   text(count, 40, 50);
-  text('gapI:', 0, 90);
-  text(gapI, 40, 90);
+  text('newX:', 0, 90);
+  text(newX, 40, 90);
   text('newY:', 0, 120);
   text(newY, 40, 120);
   text('angle', 0, 70);
   text(angle, 40, 70);
 
-  let k = n / d;
-  translate(10, height/10);
-  // diff = 0;
-
-  for(let i=0; i < width; i += gapI){
-    // rotate();
+   let k = n / d;
+  translate(width/2, height/2);
+  diff = 0;
+  for(let i=0; i<TWO_PI; i+= TWO_PI/count){
+    rotate(TWO_PI/count);
     push();
-    // translate(width/2, height/2)
-    locX = i
-    locY = map(sin(i), -1, 1, 0, height);
-    translate(locX, locY);
+    translate(newX, newY);
     beginShape(LINES);
     stroke(hue2, 50, 255);
     noFill();
     strokeWeight(strokeW);
     // k += diff;
     // for loop
-    a = angle;
     for (let a = 0; a < TWO_PI * d; a+= angle) {
-      let r = 100 * cos(k * a);
+      let r = 200 * cos(k * a);
       let x = r * cos(a);
       let y = r * sin(a);
       vertex(x, y);
     }
     endShape();
     pop();
-    // diff += 30;
+    diff += 0.25
   }
 
-}
+ }
