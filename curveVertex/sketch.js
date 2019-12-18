b2c153d9ee11a64db00a0f0983c798e8b028f197
sosunnyproject <parksosun1103@gmail.com>
2019-12-18 23:55:06 +0900

    third revision

--- curveVertex/sketch.js
+++ curveVertex/sketch.js
@@ -2,13 +2,15 @@ function preload() {
   baliImg = loadImage('bali-beach2.jpg');
 }
 
-let slider, slider2;
+let slider, slider2, slider3;
 
 function setup() {
   createCanvas(400, 400);
   baliImg.resize(400, 400);
   slider = createSlider(0, 60, 10);
   slider2 = createSlider(0, 60, 10);
+  slider3 = createSlider(0, 10, 2);
+
 }
 
 function draw() {
@@ -30,13 +32,13 @@ function draw() {
 
   let gap = 0;
   let heightGap = 15;
-  let noiseScale = 0.25;
+  let noiseScale = 0.05;
   let sliderVal = slider.value();
   let sliderVal2 = slider2.value();
-
+  let sliderVal3 = slider3.value();
   // noise
   for (let y = 3; y < height; y += heightGap) {
-    strokeWeight(1);
+    strokeWeight(3);
 
     let startX = 0;
     let startY = y;
@@ -45,11 +47,11 @@ function draw() {
     for (let x = startX+gap; x < endX - gap ; x += 1) {
       let col = baliImg.get(x, y);
       stroke(col);
-      let noiseVal = noise((frameCount + x) * noiseScale,
-                           frameCount * 0.22 * noiseScale);
+      let noiseVal = noise((frameCount*0.1 + x) * noiseScale,
+                           frameCount*0.1 + y * noiseScale);
       line(x, y + noiseVal * sliderVal, x, y + noiseVal * sliderVal2);
     }
 
-    gap += 0;  // x width control
+    gap += sliderVal3;  // x width control
   }
 }
