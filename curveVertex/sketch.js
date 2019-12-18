da0cdffec755c745c632f0671e9fd1f1154c1171
sosunnyproject <parksosun1103@gmail.com>
2019-12-18 22:59:41 +0900

    line with noise version

--- curveVertex/sketch.js
+++ curveVertex/sketch.js
@@ -1,44 +1,51 @@
-let vectorArray = [];
-let baliImg;
-
 function preload() {
   baliImg = loadImage('bali-beach1.jpg');
 }
 
 function setup() {
-  baliImg.resize(400, 400);
   createCanvas(400, 400);
-  background(baliImg);
+  baliImg.resize(400, 400);
 }
 
 function draw() {
-}
+  background(0);
+  background(baliImg);
 
-function mousePressed(){
-  pickPoint();
-}
 
-function pickPoint() {
-  stroke(0);
-  strokeWeight(3);
-  point(mouseX, mouseY);
-  // sea
-  // sun 210, 330
-  // sk 
-  let v = createVector(mouseX, mouseY);
-  vectorArray.push(v);
-  drawCurve();
-}
+  // straight line
+  /*
+    for(let y = 10; y < height; y += 10) {
+      stroke(255);
+      strokeWeight(3);
+      let startX = 0;
+      let startY = y;
+      let endX = width;
+      let endY = y;
+      line(startX + gap, startY, endX - gap, endY);
+      gap += 10;
+    }
+    */
 
-function drawCurve() {
-  noFill();
-  beginShape();
-  for(let i = 0; i < vectorArray.length; i++) {
-    let col =
-        baliImg.get(vectorArray[i].x, vectorArray[i].y);
-    stroke(col);
+  let gap = 0;
+  let heightGap = 7;
+  let noiseScale = 0.05;
+
+  // noise
+  for (let y = 3; y < height; y += heightGap) {
     strokeWeight(1);
-    curveVertex(vectorArray[i].x, vectorArray[i].y);
+
+    let startX = 0;
+    let startY = y;
+    let endX = width;
+    let endY = y;
+    for (let x = startX+gap; x < endX - gap ; x += 0.7) {
+      let col = baliImg.get(x, y);
+      stroke(col);
+      let noiseVal = noise((frameCount + x) * noiseScale,
+                           frameCount * 0.22 * noiseScale);
+      line(x, y + noiseVal * 37, x, y + noiseVal * 35);
+    }
+
+    gap += 3;
   }
-  endShape();
 }
