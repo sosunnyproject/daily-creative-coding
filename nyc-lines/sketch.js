2bbb9bb9a0cd78775a18a79df02ac42de3995afb
sosunnyproject <parksosun1103@gmail.com>
2020-01-07 21:57:52 +0900

    imgs array, shapes

--- nyc-lines/sketch.js
+++ nyc-lines/sketch.js
@@ -1,56 +1,92 @@
-let img1, img2, img3, bgImg;
+let bgImg;
 let rad = 5;
 let gap = 10;
+let imgs = [];
+let ind = 0;
+let slider1, slider2;
+let mic;
+let ellButton, rectButton;
+let col;
 
 function preload() {
-  img1 = loadImage('paris-night1.jpg');
-  img2 = loadImage('seoul-night3.jpg');
-  img3 = loadImage('budapest-night1.jpg');
+  for (let i = 0; i < 5; i++) {
+    imgs[i] = loadImage("n" + i + ".jpg");
+  }
 }
 
 function setup() {
-  bgImg = img1;
-  createCanvas(800, 600);
-  bgImg.resize(800, 600);
+  createCanvas(800, 600, WEBGL);
+  bgImg = imgs[0];
+  bgImg.resize(width, height);
+  slider1  = createSlider(0, 100, 20);
+  slider2  = createSlider(0, 50, 5);
 
-  /* for nyc1.jpg - 3 pics
-  image(bgImg, 0, 0);
-  image(bgImg, 0, bgImg.height);
-  image(bgImg, 0, bgImg.height*2);
+  // audio in
+  mic = new p5.AudioIn();
+  mic.start();
+
+  // button
+  /*
+  ellButton = createButton('elliipse');
+  rectButton = createButton('rectangle');
+  ellButton.mousePressed(changeShape('ellipse'));
+  rectButton.mousePressed(changeShape('rectangle'));
   */
 }
 
 
 function draw() {
+  // brightness threshold slider
+  let bVal = slider1.value();
+  rad = slider2.value();
+
+  // let micLevel = mic.getLevel();
+
   background(0);
+  bgImg.resize(width, height);
   if (mouseIsPressed) {
-    background(bgImg);
+    bgImg.resize(width, height);
+    let temp = image(bgImg, -width/2, -height/2);
+    // background(bgImg);
   }
 
-  for (let x = 0; x < width; x += gap) {
-    for (let y = 0; y < height; y += gap) {
+  for (let x = -width/2; x < width/2; x += gap) {
+    for (let y = -height/2; y < height/2; y += gap) {
 
-      let color = bgImg.get(x, y);
+      col = bgImg.get(x + bgImg.width/2, y + bgImg.height/2);
 
-      // color = bgImg.get(x, y - bgImg.height);
-
-      // 80: without sky
-      if (brightness(color) > 50) {
-        fill(color);
+      if (brightness(col) > bVal) {
+        col[3] = 200;
+        fill(col);
         noStroke();
-        rect(x, y, 5, rad * noise(frameCount * 0.5) * random(3));
+        ellipse(x, y, rad * noise(frameCount * 0.5), rad * noise(frameCount * 0.5));
+        // rect(x, y, rad, rad * noise(frameCount * 0.5) * random(3));
+
       }
     }
   }
 }
 
+function changeShape(){
+    ellipse(x, y, rad * noise(frameCount * 0.5), rad * noise(frameCount * 0.5));
+    rect(x, y, 5, rad * noise(frameCount * 0.5) * random(3));
+}
+
 function keyPressed() {
-  if (event.key === '1') {
-    bgImg = img1;
-  } else if (event.key === '2') {
-    console.log(keyCode);
-    bgImg = img2;
-  } else if (event.key === '3') {
-    bgImg = img3;
+  console.log(ind);
+  if(keyCode === LEFT_ARROW){
+    if(ind > 0) {
+      ind--;
+    } else {
+      ind = imgs.length - 1;
+    }
+  } else if (keyCode === RIGHT_ARROW) {
+    if(ind < imgs.length - 1) {
+      ind++;
+    } else {
+      ind = 0;
+    }
   }
+  console.log(ind);
+  bgImg = imgs[ind];
 }
