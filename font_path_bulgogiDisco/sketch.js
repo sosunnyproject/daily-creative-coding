8c1533b99a9007f1ff02bb5f1d571806c490d5ef
sosunnyproject <parksosun1103@gmail.com>
2020-02-19 20:46:14 +0900

    animation

--- font_path_bulgogiDisco/sketch.js
+++ font_path_bulgogiDisco/sketch.js
@@ -5,13 +5,13 @@ function preload() {
 
 let points;
 let bounds;
+let count = 0;
 function setup() {
   createCanvas(800, 500);
-  stroke(0);
   fill(255, 104, 204);
 
   points1 = font.textToPoints('BULGOGI', 0, 0, 100, {
-    sampleFactor: 0.5,
+    sampleFactor: 0.15,
     simplifyThreshold: 0
   });
   points2 = font.textToPoints('DISCO', 0, 0, 100, {
@@ -21,19 +21,33 @@ function setup() {
 }
 
 function draw() {
-  background(255);
-  beginShape();
+  background(0, 10); // 전 프레임의 애니메이션 궤적이 남느냐 안남느냐 결정.
   translate(50, 150);
-
   strokeWeight(5);
+
+  // animation
+  // for 문 안으로 다 들이밀면, 더 빠르게
+  stroke(255, 0, 0);
+
+  for (let i = 0; i < 10; i++) {
+    let p = points1[count];
+    point(p.x, p.y);
+    count++;
+    if (count > points1.length-1) {
+      count = 0;
+    }
+  }
+/*
+  beginShape();
   for (let i = 0; i < points1.length; i++) {
     let p = points1[i];
     point(p.x, p.y);
   }
   endShape(CLOSE);
+  */
+  stroke(255);
 
   beginShape();
-
   translate(100, 200);
   strokeWeight(5);
   for (let i = 0; i < points2.length; i++) {
