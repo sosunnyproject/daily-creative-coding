441947a42b0ca2b03a48e2416fabb1c13be34e19
sosunnyproject <parksosun1103@gmail.com>
2020-07-06 23:12:36 +0900

    sketches

--- leapMotionTest/2_1roses_fft.js
+++ leapMotionTest/2_1roses_fft.js
@@ -29,7 +29,7 @@ let n = 4;
 let hue1, hue2, strokeW, distance, angle, diff;
 
 //fft
-var fft, song, button;
+var fft, song, button, fftVal;
 
 function toggleSong() {
   if (song.isPlaying()) {
@@ -40,11 +40,11 @@ function toggleSong() {
 }
 
 function preload() {
-  // song = loadSound('song_name.mp3');
+  song = loadSound('mongle.wav');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(windowHeight, windowHeight);
   colorMode(HSB);
 
   // song play button
@@ -56,53 +56,29 @@ function setup() {
 
 function draw() {
   background(hue1, 70, 10)
-
-  // mark position 
-  /*
-  strokeWeight(2);
-  fill(0, 100, 100)
-  ellipse(rightHand.x + width/2, rightHand.y, 100);
-  fill(100,  100, 100)
-  ellipse(leftHand.x + width/2 , leftHand.y, 50);
-  */
-
+  fullscreen(true)
   // fft value
   var spectrum = fft.analyze();
+  // fft values
   for (var i = 0; i < spectrum.length; i++) {
     var amp = spectrum[i];
-    var y = map(amp, 0, 512, 0, height);
-    // previous examples
-    // fill(i*5, 200, i*10);
-    // rect(i*w,y, w - 2, height - y * 2, 5);
-    // ellipse(i*w, y , 50, height - y * 1.5)
-
     // change d, n values according to spectrum
   }
 
-  
   // motion values
-  d = map(handRad.left, -4, 4, 1, 18)
-  n = map(handRad.right, -4, 4, 1, 14)
+  d = map(handRad.left, -4, 4, 1, 18) || 1
+  n = map(handRad.right, -4, 4, 1, 14) | 4
   distance = map(Math.abs(rightHand.x - leftHand.x), 20, width, 5, 80);
   count = map(Math.floor(distance), 0, 200, 1, 30);
 
   // fixed values
   hue1 = Math.abs(cos(frameCount*0.003)*360)
   hue2 = (hue1+180)%360
-  strokeW = 1
-  angle = map(sin(frameCount*0.00008), 1, -1, 0.1, 2.5)
+  strokeW = 0.3
+  // angle = map(sin(frameCount*0.00008), 1, -1, 0.1, 2.5)
+  angle = map(spectrum[0], 0, 512, 0.1, 2.5);
 
-  textSize(12);
-  fill(200);
-  text('d:', 10, 30)
-  text(d, 50, 30)
-  text('n:', 10, 60)
-  text(n, 50, 60)
-  text('distance:', 10, 190)
-  text(distance, 100, 190)
-  text('angle', 10, 270)
-  text(angle, 50, 270)
-
+  // rose shape
   let k = (n / d)
   push()
   translate(width/2, height/2);
@@ -125,8 +101,33 @@ function draw() {
     diff += 0.25
   }
   pop()
+
+  textSize(12);
+  fill(200);
+  text('d:', 10, 30)
+  text(d, 50, 30)
+  text('n:', 10, 60)
+  text(n, 50, 60)
+  text('distance:', 10, 190)
+  text(distance, 100, 190)
+  text('angle', 10, 270)
+  text(angle, 50, 270)
 }
 
 function drawHandPos(){
+  // mark hand coordinates 
+  noStroke();
+  push()
+  fill(200, 20, 20)
+  translate(rightHand.x, rightHand.y);
+  rotate(n);
+  rect(0, 0, 10, 50);
+  pop()
 
+  push()
+  fill(200,  20, 20)
+  translate(leftHand.x, leftHand.y);
+  rotate(d);
+  rect(0, 0, 10, 50);
+  pop()
 }
