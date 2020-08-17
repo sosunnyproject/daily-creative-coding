68ec9f7a8a0e0c0aa74a710d790b472af0db6644
sosunnyproject <parksosun1103@gmail.com>
2020-08-17 21:27:27 +0900

    colors

--- bezierPatterns/sketchMouse.js
+++ bezierPatterns/sketchMouse.js
@@ -5,28 +5,32 @@
 let points = []
 let anchors = []
 let t, c;
+let colors = []
 
 function setup() {
-  c = createCanvas(400, 400);
-  
+  c = createCanvas(500, 500);
+  colorMode(HSB)
+  for(let i=0; i<10000; i++){
+    colors.push(random(360))
+  }
 }
 
 function setPoints(x, y) {
       let pointS = { x: x, y: y}
       let pointE = {
-        x: pointS.x + random(-25, 25),
-        y: pointS.y + random(-25, 25)
+        x: pointS.x + random(-10, 10),
+        y: pointS.y + random(-10, 10)
       }
       points.push(pointS)
       points.push(pointE)
 
       let anchorS = {
-        x: pointS.x + random(25, 50),
-        y: pointS.y + random(-100, 100),
+        x: pointS.x + random(20, 30),
+        y: pointS.y + random(-50, 50),
       }
       let anchorE = {
-        x: pointE.x + random(-25, -50),
-        y: pointE.y + random(-100, 100),
+        x: pointE.x + random(-20, -30),
+        y: pointE.y + random(-50, 50),
       }
       anchors.push(anchorS)
       anchors.push(anchorE)
@@ -37,10 +41,12 @@ function draw() {
   background(255)
   
   noFill()
-  stroke(0)
+  // stroke(0)
   // stroke(sin(t)*300, 40, 80)
-  strokeWeight(2)
+  strokeWeight(5)
+  
   for (let i = 0; i < points.length; i += 2) {
+    stroke(colors[i], 100, 100)
     drawBezier(points[i], anchors[i], anchors[i + 1], points[i + 1])    
   }
 }
@@ -52,7 +58,11 @@ function drawBezier(pointS, anchorS, anchorE, pointE) {
     pointE.x, pointE.y)
 }
 
-function mouseClicked() {
+// function mouseClicked() {
+//   setPoints(mouseX, mouseY)
+// }
+
+function mouseDragged() {
   setPoints(mouseX, mouseY)
 }
 
