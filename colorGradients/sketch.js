1428cd8b4413a8b34413f2aeb047b65b61043208
sosunnyproject <parksosun1103@gmail.com>
2020-09-14 23:29:09 +0900

    'add_bezier'

--- colorGradients/sketch.js
+++ colorGradients/sketch.js
@@ -1,6 +1,6 @@
 // ref: https://www.openprocessing.org/sketch/942270
 
-let palette = ['#3399CC', '#67B8DE', '#91C9E8', '#B4DCED','#E8F8FF' ];
+let palette = ['#3399CC', '#67B8DE', '#91C9E8', '#B4DCED','#E8F8FF' ]
 
 function setup() {
   createCanvas(400, 400)
@@ -13,7 +13,7 @@ function setup() {
 function draw() {
   background(0, 0, 90)
 
-  noStroke()
+  
   drawingContext.shadowColor = color(0, 0, 0,60)
   drawingContext.shadowBlur = 11
 
@@ -21,10 +21,11 @@ function draw() {
   // CanvasGradient ctx.createRadialGradient(x0, y0, r0, x1, y1, r1);
   // https://p5js.org/examples/color-radial-gradient.html
 
-  for (let i = 0; i < width+20; i+= 20) {
-    for (let j = 0; j < height+20; j+= 20) {
-      let linearGrad = drawingContext.createLinearGradient(i+random(-10, -1), j+random(-10, -1), i+random(-3,6), j+random(-3,7))
-      let radialGrad = drawingContext.createRadialGradient(i+random(-10, -1), j+random(-10, -1), 3 , i+random(-3,6), j+random(-3,7), 6)
+  for (let i = 0; i < width+20; i+= 30) {
+    for (let j = 0; j < height+20; j+= 30) {
+      let linearGrad = drawingContext.createLinearGradient(i+random(-10, -1), j+random(-10, -1), i+random(-10, 30), j+random(-20, 40))
+//       let radialGrad = drawingContext.createRadialGradient(i+random(-10, -1), j+random(-10, -1), 3 , i+random(-3,6), j+random(-3,7), 6)
+      noStroke()
       
       // assign color palette
       palette = shuffle(palette, true);
@@ -33,16 +34,37 @@ function draw() {
       while (c1 == c2) {
         c2 = random(palette);
       }
-      linearGrad.addColorStop(0, c2);
-      linearGrad.addColorStop(1, c1);
       drawingContext.fillStyle = linearGrad;
       
-      let ratio = 1;
+      linearGrad.addColorStop(0, c2);
+      linearGrad.addColorStop(1, c1);
       
-      ellipse(i, j, 20)
-      // rect(i, j, 15, 15)
+      let ratio = 1;
+      let rand = int(random(0, 4))
+
+      drawShape(rand, i, j)
     }
   }
 
   frameRate(0.8)
+}
+
+function drawShape(rand, i, j){
+  if(rand === 0) {
+         bezier(i+random(10, 40), j+random(10, 40), 
+           random(i-30, i+50), random(j-30, j+50),  
+           random(i-30, i+50), random(j-20, j+40), 
+           i+random(-10,40), j+random(-10,40))
+  } else if (rand === 1){
+    ellipse(i+10, j+10, 25, 25) 
+  } else if(rand === 2){
+                 bezier(i+random(10, 40), j+random(10, 40), 
+           random(i-30, i+50), random(j-30, j+50),  
+           random(i-30, i+50), random(j-20, j+40), 
+           i+random(-10,40), j+random(-10,40))
+    // rect(i+10, j, 20, 20, 5) 
+  } else if(rand===3){
+    triangle(i, j+26, i+15, j, i+30, j+26)
+  }
+  
 }
