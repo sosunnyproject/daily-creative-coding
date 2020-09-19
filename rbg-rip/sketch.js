55c571db35c944b54132a2d9bbfd9ef3f78dc940
sosunnyproject <parksosun1103@gmail.com>
2020-09-19 12:45:27 +0900

    'ruth-ginsburg'

---
+++ rbg-rip/sketch.js
@@ -0,0 +1,34 @@
+let img;
+let pixels = []
+function preload() {
+  img = loadImage('rbg-pic.jpg');
+
+}
+
+function setup() {
+  createCanvas(img.width, img.height);
+  image(img, 0, 0)
+
+  for (let i = 0; i < width; i++) {
+    for (let j = 0; j < height; j++) {
+      let c = img.get(i, j)
+      if (c[2] < 150) {
+        pixels.push({x: i, y: j})
+      }
+    }
+  }
+
+  background(255)
+  for (let i = 0; i < pixels.length; i++) {
+      stroke(0, 0, 255)
+      ellipse(pixels[i].x, pixels[i].y, 1)
+    }
+}
+
+function draw() {
+
+
+//   console.log(mouseX, mouseY)
+
+  
+}
