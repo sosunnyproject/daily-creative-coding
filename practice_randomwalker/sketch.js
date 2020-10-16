a50252368d137053c4542fce932a7bf59a8e075b
sosunnyproject <parksosun1103@gmail.com>
2020-10-16 22:32:40 +0900

    '10/16'

--- practice_randomwalker/sketch.js
+++ practice_randomwalker/sketch.js
@@ -7,18 +7,26 @@ let walker
 function setup(){
   createCanvas(600, 600)
   w = (width - margin)/ numW
-  h = (height-margin)/ numH
+  h = (height - margin)/ numH
 
   walker = new Walker(numW, numH)
 
-  for(let i = 0; i < numW*numH; i++){
-    let x = i % numW
-    let y = floor(i /  numH) 
-
-    rect(x * w + margin *0.5, y * h + margin*0.5, w, h)
-  }
 }
 
 function draw(){
+  background(255)
+  fill(255)
+  
+  for(let i = 0; i < numW*numH; i++){
+    let x = i % numW
+    let y = floor(i /  numH) 
+    rect(x * w + margin *0.5, y * h + margin*0.5, w, h)
+  }
   walker.render()
+}
+
+function keyPressed(){
+  if(key === ' ') {
+    walker.update()
+  }
 }
--- practice_randomwalker/walker.js
+++ practice_randomwalker/walker.js
@@ -8,27 +8,52 @@ class Walker{
     this.numH = numHeight
   }
 
-
   render(){
     fill(255, 0, 255)
     ellipse(this.x * w + margin * 0.5, this.y * h + margin * 0.5, 10, 10)
   }
 
   update(){
-    let pick = floor(random(4)) // 0,1, 2, 3
-    switch(pick){
-      case 0:
-        if(this.y !== numH) this.y += 1
-        break;
-      case 1:
-        if(this.y !== 0) this.y -= 1
-        break;
-      case 2:
-        this.x += 1
-        break;
-      case 3:
-        this.x -= 1
-        break
+    // const pick = floor(random(4)) // 0,1, 2, 3
+    let pick = []
+    // vector in 4 different directions
+    // left
+    const leftVec = createVector(-1, 0)
+    // up
+    const upVec = createVector(0, 1)
+    // right
+    const rightVec = createVector(1, 0)
+    // down
+    const downVec = createVector(0, -1)
+
+    // 4가지 방향에 대한 정보를 리스트에 저장
+    // 특정 방향으로 이동이 가능한지 먼저 확인
+    // list에 방향에 대한 정보를 더해줄지 결정
+    if(this.x < this.numW) {
+      // to right
+      pick.push(rightVec)
+    } 
+    if(this.y < this.numH){
+      // to up
+      pick.push(upVec)
+    } 
+    if(this.x > 0) {
+      // to left
+      pick.push(leftVec)
     }
+    if(this.y > 0) {
+      // to down
+      pick.push(downVec)
+    }
+
+    // list 길이 만큼의 랜덤한 양의 점수 뽑기
+    const rand = floor(random(pick.length))
+
+    // 위에서 뽑힌 숫자를 list 에서 참조할 목차번호로 활용
+    const dirVector = pick[rand]
+
+    // 뽑힌 방향대로 이동
+    this.x += dirVector.x
+    this.y += dirVector.y
   }
 }
