c82979ed6b0f24a71c3e1b63713850f98b3b71eb
sosunnyproject <parksosun1103@gmail.com>
2021-01-06 23:49:27 +0900

    breath app second version

--- youtube/breath-app/sketch.js
+++ youtube/breath-app/sketch.js
@@ -1,23 +1,24 @@
 //https://editor.p5js.org/sosunnyproject/sketches/1JttzgMgR
-let count = 5
+let count = 10
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(0);
+  background(0, 50);
   angleMode(RADIANS)
+  
   noStroke()
-  let speed = sin(frameCount/100)
-  let speedY = Math.abs(cos(frameCount/120))
+  let speed = map(sin(frameCount/100), -1, 1, 0, 1)
+  let speedY = map(sin(frameCount/80), -1, 1, 0, 1)
   translate(width/2, height/2)
+  
   for(let i=0; i<TWO_PI; i+= TWO_PI/count){
-    
     rotate(TWO_PI/count);
     push()
-    fill(173,217,215, 50)
-    translate(3*TWO_PI*speed, 3*TWO_PI*speedY)
-    ellipse(0, 0, 70, 70)
+    fill(173,217+(speed*10),215+(i*8), 50)
+    translate(6*TWO_PI*speed, 3*TWO_PI*speedY)
+    ellipse(0, 0, 1000/count, 1000/count)
     pop()
   }
 }
