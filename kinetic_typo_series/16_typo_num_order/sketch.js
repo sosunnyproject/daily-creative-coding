e7d0e30c6c1f29e20e18aa7d34b40f2b9411866f
sosunnyproject <parksosun1103@gmail.com>
2021-01-12 20:35:12 +0900

    update code

--- kinetic_typo_series/16_typo_num_order/sketch.js
+++ kinetic_typo_series/16_typo_num_order/sketch.js
@@ -1,64 +1,69 @@
 let font;
-let points;
+let points=[];
 let bounds;
 let gap = 5
 let count = 0
+let words = ['헤', '아', '릴', '수', '없', '어']
+let wordcount = 0
+let sFactor = 0.02
+let factorSlider;
 
 function preload() {
-  font = loadFont("Bazzi.ttf")
+  font = loadFont("nationalMuseumL.otf")
 }
 
 function setup() {
   createCanvas(800, 800);
   stroke(0);
+  // angleMode(DEGREES)
+  factorSlider = createSlider(0, 255, 100);
   // fill(255, 104, 204);
-
-  points = font.textToPoints(' 헤', -20, height / 4 * 3, 600, {
-    sampleFactor: 0.04,
-    simplifyThreshold: 0
-  });
-  // bounds = font.textBounds(' 는 ', 0, 0, 10);
-  for (let i = 0; i < points.length; i++) {
-    points[i].num = i
-  }
   textFont(font)
-  textSize(20)
-  // colorMode(HSB)
+  // changeText(words[0])
 }
 
 function draw() {
+  let factorVal = factorSlider.value()
+  sFactor = atan(frameCount%50) * map(factorVal, 0, 255, 1.0, 0.03)
+  textSize(15)
   background(0, 10);
   // group(0, 300)
   // stroke(255)
-  fill(255)
-  let speed = int(-cos(frameCount/100)+5) ;
-  for (let i = 0; i < speed; i ++) { 
-    let nPoint = points[(count + i) % points.length];
-    fill(`hsl(${frameCount % 360}, 100%, 60%)`)
-    // fill(`hsb((${frameCount} % 360), 100, 100)`)
-    text(nPoint.num, nPoint.x, nPoint.y)
-    count++
-  }
-  for(let i = 0; i < points.length;i++){
-    // text(points[i].num, points[i].x, points[i].y)
+  noStroke()
+  let speed = int(sin(frameCount/100)+35);
+  for (let i = 0; i < speed; i+=1) { 
+    if(points.length > 0) {
+      let nPoint = points[(count+i) % points.length];
+      fill(`hsl(${(mouseX*mouseY) % 360}, 100%, 60%)`)
+      // fill(`hsb((${frameCount} % 360), 100, 100)`)
+      text(nPoint.num, nPoint.x, nPoint.y)
+      count++
+    }
   }
 }
 
-function group(startInd, endInd) {
-  noFill()
-  stroke(255)
-  beginShape()
-  // console.log(startInd, endInd)
-  vertex(points[startInd].x, points[startInd].y)
-  vertex(points[endInd].x, points[endInd].y)
-
-  for (let i = startInd; i < endInd; i++) {
-    let p = points[i];
-    vertex(
-      p.x,
-      p.y
-    );
-  }
-  endShape()
-
+function render(p) {
+  console.log(p)
 }
+function mouseClicked(){
+    points = []
+  changeText(words[wordcount])
+  if(wordcount == words.length - 1){
+    wordcount = 0
+  } else {
+    wordcount++
+  }
+}
+
+
+function changeText(word) {
+  console.log(sFactor,  atan(frameCount/1000) )
+  points = font.textToPoints(word, 100, height-100, 600, {
+    sampleFactor: sFactor,
+    simplifyThreshold: 0
+  });
+  for (let i = 0; i < points.length; i++) {
+    points[i].num = i
+  }
+}
+
