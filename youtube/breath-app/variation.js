c488c54d88b781433948713f3239503787ca0e7a
sosunnyproject <parksosun1103@gmail.com>
2021-01-14 23:55:31 +0900

    breath app logo p5js variation

--- youtube/breath-app/variation.js
+++ youtube/breath-app/variation.js
@@ -16,22 +16,22 @@ function draw() {
   noStroke()
   strokeWeight(1)
   for(let i=0; i<TWO_PI; i+= TWO_PI/count){
-    // rotate(TWO_PI/count);
-    // push()
-    // fill(frameCount%360, speed, 100, 100)
-    // // fill(173,217+(speed*20),215+(i*12), 40)
-    // translate(speed*PI, TWO_PI*speedY%50)
-    // ellipse(0, 0, 500/count, 500/count)
-    // pop()
+    rotate(TWO_PI/count);
+    push()
+    fill(frameCount%360, speed, 100, 100)
+    // fill(173,217+(speed*20),215+(i*12), 40)
+    translate(speed*PI, TWO_PI*speedY%50)
+    ellipse(0, 0, 500/count, 500/count)
+    pop()
   }
   
   for(let i = 0; i < count; i++){
-    rotate(TWO_PI/count*frame);
-    push()
-    // fill(speedY/5, speedY, 255)
-    stroke(225, speedY*220, frameCount%255)
-    translate(TWO_PI*speed, 10*PI*speedY)
-    ellipse(i, 0, 1600/count, 160/count)
-    pop()
+    // rotate(TWO_PI/count*frame);
+    // push()
+    // // fill(speedY/5, speedY, 255)
+    // stroke(225, speedY*220, frameCount%255)
+    // translate(TWO_PI*speed, 10*PI*speedY)
+    // ellipse(i, 0, 1600/count, 160/count)
+    // pop()
   }
 }
