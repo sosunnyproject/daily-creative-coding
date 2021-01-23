5696fd94dc7315acc58e4c646bf40649973aea91
sosunnyproject <parksosun1103@gmail.com>
2021-01-23 23:17:28 +0900

    interactive to touch and mouse positions

--- kinetic_typo_series/typo-star-image/tsa.js
+++ kinetic_typo_series/typo-star-image/tsa.js
@@ -1,10 +1,12 @@
 // travelling salesperson #1 : https://www.youtube.com/watch?v=BAejnwN4Ccw
 
 let stars = []
-totalStars = 50
-const arrNum = 40 // number of small array groups inside big 'stars' total array
+totalStars = 10
+let arrNum = 20 // number of small array groups inside big 'stars' total array
 let recordDistance = [], bestStars = []
 let off = 0.01 //var for noise
+let randOffs = []
+
 
 let particles = [] // 생성할 파티클들을 담을 리스트
 let particleNum = 0 // 생성될 파티클의 개수, 가장처음에는 0개이므로 0.
@@ -21,52 +23,67 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600)
+  createCanvas(windowWidth, windowWidth)
   background(0)
   colorMode(HSB, 100)
-  particles = font.textToPoints('별', 0, 500, 600, {
+  particles = font.textToPoints('별', 0, windowWidth-100, windowWidth, {
     sampleFactor: 5,  //higher, the more precise
     simplifyThreshold: 0
   });
-
+  // for(let i = 0; i < arrNum; i++) {
+  //   randOffs.push(random(-20, 20))
+  // }
   for(let i = 0; i < arrNum; i++) {
     setStars(i)
     recordDistance.push(calcDistance(stars[i]))
     bestStars.push(stars[i].slice())
   }
-  console.log(stars)
   // recordDistance = calcDistance(stars)
   // bestStars = stars.slice()
 }
 
 
 function draw() {
-  background(0, 100)
+  background(0, 40)
   stroke(255)
   noFill()
-  if(frameCount%20 == 0){
-    for(let a = 0; a < arrNum; a++) {
-      // for(let s = 0; s < totalStars; s++ ){
-        setStars(a)
-        // stars[a][s].x = sin(frameCount/50) * random(-2, 2) + stars[a][s].x
-        // stars[a][s].y = cos(frameCount/50) * random(-2, 2) - stars[a][s].y
-        // off += 0.2
-      // }
-    }
+  frameRate(7)
+  // if(frameCount%2 == 0){
+  // if(randOffs.length > arrNum){
+  //   randOffs = []
+  // }
+  // for(let i = 0; i < arrNum; i++) {
+  //   randOffs.push(random(-20, 20))
+  // }
+  for(let a = 0; a < arrNum; a++) {
+    // for(let s = 0; s < totalStars; s++ ){
+      setStars(a)
+      // stars[a][s].x = sin(frameCount/50) * random(-2, 2) + stars[a][s].x
+      // stars[a][s].y = cos(frameCount/50) * random(-2, 2) - stars[a][s].y
+      // off += 0.2
+    // }
   }
-
+  // }
   for(let j = 0; j < arrNum; j++){
-    beginShape()
+    if(stars.length > j) {
       for(let i = 0; i < stars[j].length; i++){
-        strokeWeight(1)
         stroke(12, 100, 100)
-        drawStart && ellipse(stars[j][i].x, stars[j][i].y, 6, 6)
-        drawStart ? strokeWeight(0.25) : strokeWeight(0.5)
-        let m = map(stars[j][i].y*stars[j][i].x, 0, 1000000, 0, 360) 
-        stroke(m, 80, 100)
-        vertex(stars[j][i].x, stars[j][i].y)
+        // 터치하면, 터치된 영역 근처만 별이 반짝거리도록.
+        if(drawStart) { 
+          let currentStar = stars[j][i]
+          if((mouseX-150 < currentStar.x) && (currentStar.x < mouseX+150)){
+            if((mouseY-150 < currentStar.y) && (currentStar.y < mouseY+150)){
+              ellipse(stars[j][i].x, stars[j][i].y, random(10, 20))
+            } else {
+              stroke(0)
+            }
+          } else {
+            stroke(0)
+          }
+        }
       }
-    endShape()
+      if(stars[j].length >0) drawDistance(stars[j])
+    }
   }
   
 
@@ -111,7 +128,7 @@ function swap(arr, i, j){
 
 // input: array
 // output: calcuate distance over the elements in array
-function calcDistance(arr){
+function calcDistance(arr){     
   let sum = 0
   for(let i = 0; i < arr.length - 1; i++){
     let d = dist(arr[i].x, arr[i].y,
@@ -121,6 +138,19 @@ function calcDistance(arr){
   return sum
 }
 
+function drawDistance(arr){
+  for (let i = 0; i < arr.length/2; i++) {
+    let a = random(arr)
+    let b = random(arr)
+    const curr = arr[i]
+    while(dist(curr.x, curr.y, b.x, b.y) > 50){
+      b = random(arr)
+    }
+    strokeWeight(3)
+    // line(dots[i].x, dots[i].y, a.x, a.y)
+    line(curr.x, curr.y, b.x, b.y)
+  }
+}
 
 // sampleFactor: 5
 // ㅂ : 0, particles.length/4,
@@ -128,7 +158,9 @@ function calcDistance(arr){
 // ㅕ : 2 * particles.length/3, particles.length-25
 // 나머지는 ㅂ 안의 ㅁ 구역.
 function setStars(n) {
-  if(stars.length === arrNum){
+  arrNum = floor(map(mouseX, 0, width, 5, 40))
+  totalStars = floor(map(mouseY, 0, height, 5, 20))
+  if(stars.length >= 100){
     stars = []
   }
   /*
@@ -141,14 +173,20 @@ function setStars(n) {
     endInd = particles.length
   }
   */
+
   const divider = (particles.length)/arrNum
   let smallStars = []
   for (let i = 0; i < totalStars; i++){
     const startInd = n * divider
     const endInd = (n+1) * divider
+    
     let ind = floor(random(startInd, endInd))  // ㅂ 좌표
     let p = particles[ind]
-    let v = createVector(p.x, p.y)
+    let x = p.x + (random(-10, 20) * sin(frameCount/10))
+    let y = p.y + (random(-20, 10) )
+    off += 10 
+
+    let v = createVector(x, y)
     smallStars.push(v)
     // stars[i] = v;
   }
@@ -158,6 +196,7 @@ function setStars(n) {
 function touchStarted(e){
   // console.log(e)
   drawStart = true
+  e.preventDefault();
   
 }
 function touchEnded(){
