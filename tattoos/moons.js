3447b939dedd4a8ea8cae84c27da6e99c77eaf52
sosunnyproject <parksosun1103@gmail.com>
2021-01-27 23:56:17 +0900

    add moon tattoo sketch

---
+++ tattoos/moons.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+  translate(100, 100)
+  noFill()
+  stroke(0)
+  blendMode(HARD_LIGHT)
+  ellipse(0, 0, 80, 80)
+  // fill(255)
+  noFill()
+  noStroke()
+  stroke(0)
+  ellipse(20, 0, 80, 80)
+}
+
+function draw() {
+  // background(220);
+
+}
