14dd9640964f7dc190d6d3bd0ab5d3e6de0071b6
sosunnyproject <parksosun1103@gmail.com>
2021-02-01 23:55:36 +0900

    add variation sketch

---
+++ youtube/nike-logo/variation.js
@@ -0,0 +1,178 @@
+let points = []
+let mousePress = false
+let hideLines = false
+let anchor1 = {
+  x: 160,
+  y: 176
+}
+let control1 = {
+  x: 41,
+  y: 333
+}
+let control2 = {
+  x: 270,
+  y: 235
+}
+let anchor2 = {
+  x: 570,
+  y: 226
+}
+
+let b_anchor1 = {
+  x: anchor1.x,
+  y: anchor1.y
+}
+
+let b_control1 = {
+  x: 41,
+  y: 533
+}
+
+let b_control2 = {
+  x: 170,
+  y: 335
+}
+
+let b_anchor2 = {
+  x: anchor2.x,
+  y: anchor2.y
+}
+
+function setup() {
+  createCanvas(600, 600);
+
+}
+//bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+// anchor1, control1, control2, anchor2
+function draw() {
+  background(0, 20);
+  noFill()
+  /*
+  b_anchor1 = {
+    x: anchor1.x,
+    y: anchor1.y
+  }
+  b_anchor2 = {
+    x: anchor2.x,
+    y: anchor2.y
+  }
+  */
+
+  points = []
+  let f = frameCount / 30
+  if (mousePress) {
+    if (checkRange(anchor1)) {
+      anchor1.x = mouseX
+      anchor1.y = mouseY
+    }
+    if (checkRange(control1)) {
+      control1.x = mouseX
+      control1.y = mouseY
+    }
+    if (checkRange(control2)) {
+      control2.x = mouseX
+      control2.y = mouseY
+    }
+    if (checkRange(b_control1)) {
+      b_control1.x = mouseX
+      b_control1.y = mouseY
+    }
+    if (checkRange(b_control2)) {
+      b_control2.x = mouseX
+      b_control2.y = mouseY
+    }
+  }
+  points.push(anchor1)
+  points.push(control1)
+  points.push(control2)
+  points.push(anchor2)
+  points.push(b_anchor1)
+  points.push(b_control1)
+  points.push(b_control2)
+  points.push(b_anchor2)
+  
+  stroke(255);
+  fill(255)
+  beginShape()
+  vertex(anchor1.x + sin(f)*50, anchor1.y+ sin(f)*50)
+  bezierVertex(
+    control1.x + sin(f)*50, control1.y + sin(f)*50,
+    control2.x + sin(f)*50, control2.y + sin(f)*50,
+    anchor2.x + cos(f)*50, anchor2.y+ cos(f)*50)
+  bezierVertex(
+    b_control2.x+ cos(f)*50, b_control2.y+ cos(f)*50,
+    b_control1.x+ tan(f)*50, b_control1.y+ tan(f)*50,
+    anchor1.x+ sin(f)*50, anchor1.y+ sin(f)*50)
+  endShape()
+  
+  stroke(255, 102, 0);
+  strokeWeight(2)
+  noFill()
+  if(!hideLines){
+    line(anchor1.x, anchor1.y, control1.x, control1.y)
+    line(control2.x, control2.y, anchor2.x, anchor2.y)
+    for(let i = 0; i < points.length; i++) {
+    ellipse(points[i].x, points[i].y, 10)
+    }
+  }
+  
+  /*
+  bezier(anchor1.x, anchor1.y,
+    control1.x, control1.y,
+    control2.x, control2.y,
+    anchor2.x, anchor2.y);
+  bezier(b_anchor1.x, b_anchor1.y,
+    b_control1.x, b_control1.y,
+    b_control2.x, b_control2.y,
+    b_anchor2.x, b_anchor2.y);
+  
+  */
+}
+
+function mousePressed() {
+  console.log(mouseX, mouseY)
+  mousePress = true
+}
+
+function mouseReleased() {
+  if (checkRange(anchor1)) {
+    anchor1.x = mouseX
+    anchor1.y = mouseY
+  }
+  if (checkRange(control1)) {
+    control1.x = mouseX
+    control1.y = mouseY
+  }
+  if (checkRange(control2)) {
+    control2.x = mouseX
+    control2.y = mouseY
+  }
+  if (checkRange(b_control2)) {
+    b_control2.x = mouseX
+    b_control2.y = mouseY
+  }
+  if (checkRange(b_control1)) {
+    b_control1.x = mouseX
+    b_control1.y = mouseY
+  }
+
+  mousePress = false
+  return false
+}
+
+function checkRange(point) {
+  if (
+    (point.x - 20 < mouseX) &&
+    (mouseX < point.x + 20) &&
+    (point.y - 20 < mouseY) &&
+    (mouseY < point.y + 20)) {
+    return true
+  }
+  return false
+}
+
+function keyPressed(){
+  if(keyCode === 32){
+    hideLines = !hideLines
+  }
+}
