f964fe56fabc45ee035f8712bbf19bdf2a7a8c86
sosunnyproject <parksosun1103@gmail.com>
2021-02-12 23:33:37 +0900

    add movement to points

--- youtube/nike-logo/sketch.js
+++ youtube/nike-logo/sketch.js
@@ -44,21 +44,11 @@ function setup() {
   createCanvas(600, 600);
 
 }
-//bezier(x1, y1, x2, y2, x3, y3, x4, y4)
+// bezier(x1, y1, x2, y2, x3, y3, x4, y4)
 // anchor1, control1, control2, anchor2
 function draw() {
   background(0, 20);
   noFill()
-  /*
-  b_anchor1 = {
-    x: anchor1.x,
-    y: anchor1.y
-  }
-  b_anchor2 = {
-    x: anchor2.x,
-    y: anchor2.y
-  }
-  */
 
   points = []
   let f = frameCount / 30
@@ -66,6 +56,10 @@ function draw() {
     if (checkRange(anchor1)) {
       anchor1.x = mouseX
       anchor1.y = mouseY
+    }
+    if (checkRange(anchor2)) {
+      anchor2.x = mouseX
+      anchor2.y = mouseY
     }
     if (checkRange(control1)) {
       control1.x = mouseX
@@ -85,54 +79,64 @@ function draw() {
     }
   }
   points.push(anchor1)
+  points.push(anchor2)
   points.push(control1)
   points.push(control2)
-  points.push(anchor2)
-  points.push(b_anchor1)
   points.push(b_control1)
   points.push(b_control2)
-  points.push(b_anchor2)
+
+  // anchor1.x += sin(frameCount / 50)
+  // anchor1.y += sin(frameCount / 50)
+  // anchor2.x += sin(frameCount / 50)
+  // anchor2.y += sin(frameCount / 50)
+  control1.x += sin(frameCount / 10) * 10
+  control1.y += sin(frameCount / 10) * 10
+  control2.x += sin(frameCount / 50)
+  control2.y += sin(frameCount / 50)
+  b_control2.x += sin(frameCount / 50)
+  b_control2.y += sin(frameCount / 50)
+  b_control1.x += sin(frameCount / 50)
+  b_control1.y += sin(frameCount / 50)
   
   stroke(255);
   fill(255)
+  drawBezierCurves()
+
+  stroke(255, 102, 0);
+  strokeWeight(2)
+  noFill()
+  drawPoints()
+
+}
+
+function drawPoints() {
+  if (!hideLines) {
+    line(anchor1.x, anchor1.y, control1.x, control1.y)
+    line(control2.x, control2.y, anchor2.x, anchor2.y)
+    for (let i = 0; i < points.length; i++) {
+      ellipse(points[i].x, points[i].y, 30)
+    }
+  }
+}
+
+function drawBezierCurves() {
   beginShape()
-  vertex(anchor1.x, anchor1.y)
+  vertex(anchor1.x,
+    anchor1.y)
   bezierVertex(
-    control1.x, control1.y,
-    control2.x, control2.y,
+    control1.x,
+    control1.y,
+    control2.x,
+    control2.y,
     anchor2.x, anchor2.y)
   bezierVertex(
     b_control2.x, b_control2.y,
     b_control1.x, b_control1.y,
     anchor1.x, anchor1.y)
   endShape()
-  
-  stroke(255, 102, 0);
-  strokeWeight(2)
-  noFill()
-  if(!hideLines){
-    line(anchor1.x, anchor1.y, control1.x, control1.y)
-    line(control2.x, control2.y, anchor2.x, anchor2.y)
-    for(let i = 0; i < points.length; i++) {
-    ellipse(points[i].x, points[i].y, 10)
-    }
-  }
-  
-  /*
-  bezier(anchor1.x, anchor1.y,
-    control1.x, control1.y,
-    control2.x, control2.y,
-    anchor2.x, anchor2.y);
-  bezier(b_anchor1.x, b_anchor1.y,
-    b_control1.x, b_control1.y,
-    b_control2.x, b_control2.y,
-    b_anchor2.x, b_anchor2.y);
-  
-  */
 }
 
 function mousePressed() {
-  console.log(mouseX, mouseY)
   mousePress = true
 }
 
@@ -164,17 +168,17 @@ function mouseReleased() {
 
 function checkRange(point) {
   if (
-    (point.x - 20 < mouseX) &&
-    (mouseX < point.x + 20) &&
-    (point.y - 20 < mouseY) &&
-    (mouseY < point.y + 20)) {
+    (point.x - 30 < mouseX) &&
+    (mouseX < point.x + 30) &&
+    (point.y - 30 < mouseY) &&
+    (mouseY < point.y + 30)) {
     return true
   }
   return false
 }
 
-function keyPressed(){
-  if(keyCode === 32){
+function keyPressed() {
+  if (keyCode === 32) {
     hideLines = !hideLines
   }
 }
