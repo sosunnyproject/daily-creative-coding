9856df2e77d9f20abe33ca06cc7a375687d7a365
sosunnyproject <parksosun1103@gmail.com>
2021-02-28 23:37:00 +0900

    add kinect, kinectron data file

---
+++ kinect/sketch.js
@@ -0,0 +1,58 @@
+var myCanvas = null;
+var mode = -1;
+
+// Declare kinectron 
+var kinectron = null;
+
+function setup() {
+  canvas = createCanvas(500, 500);
+  background(0);
+
+  // Define and create an instance of kinectron
+  kinectron = new Kinectron("172.16.229.215");
+
+  // CONNECT TO MIRCROSTUDIO
+  //kinectron = new Kinectron("kinectron.itp.tsoa.nyu.edu");
+
+  // Connect with application over peer
+  kinectron.makeConnection();
+
+  // Set callbacks
+  kinectron.setRGBCallback(drawFeed);
+  kinectron.setDepthCallback(drawFeed);
+  kinectron.setInfraredCallback(drawFeed);
+}
+
+function draw() {
+
+}
+
+// Choose camera to start based on key pressed
+function keyPressed() {
+  if (keyCode == TAB) {
+    mode++;
+    mode%=3;
+    //kinectron.stopAll();
+    background(0);
+  }
+  switch(mode){
+    case 0:
+	    kinectron.startRGB();
+      break;
+    case 1:      	
+    	kinectron.startDepth();
+      break;
+    case 2:
+      kinectron.startInfrared();
+      break;
+  }
+}
+
+function drawFeed(img) {
+    console.log(kinectron);
+
+  // Draws feed using p5 load and display image functions  
+  loadImage(img.src, function(loadedImage) {
+    image(loadedImage, 0, 0);
+  });
+}
