ee14eb85555a5f928d91abf5e4dfe8b52e32d8e1
sosunnyproject <parksosun1103@gmail.com>
2021-03-13 22:22:46 +0900

    add gyroscope sketch

---
+++ gyroscope/sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(225, 0, 0);
+  // beta - x axis
+  let ox = document.getElementById('Orientation_b').innerText
+  textSize(30)
+  text(ox, 50, 50)
+  let oxNum = Number.parseInt(Math.floor(ox))
+  let oxcoord = map(oxNum, -80, 80, 0, height)
+  
+  // gamma y-axis
+  let oy = document.getElementById('Orientation_g').innerText
+  let oyNum = Number.parseInt(Math.floor(oy))
+  let oycoord = map(oyNum, -100, 100, 0, width)
+
+  ellipse(oycoord, oxcoord, 40)
+}
