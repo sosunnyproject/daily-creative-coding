d597322df562d426af139f5f5f20722af5c6d0a5
sosunnyproject <parksosun1103@gmail.com>
2022-05-07 09:52:04 +0900

    differentiate text area for noise particles

--- 220502_flowfieldAudio/sketch2.js
+++ 220502_flowfieldAudio/sketch2.js
@@ -1,10 +1,10 @@
-var inc = 0.1; //increment
-var scl = 10; //scale
+var inc = 0.25; //increment
+var scl = 20; //scale
 var cols, rows;
 var zoff = 0;
-var fr; //framerate
 var particles = [];
 var flowfield;
+let flowfieldText = []
 
 let mic, fft, sound;
 let spectrum;
@@ -12,34 +12,69 @@ let spectrum;
 let vol;
 let spec = 4;
 
+let textPg;
+
 function setup(){
   createCanvas(windowWidth, windowHeight);
   cols = floor(width/scl);
   rows = floor(height/scl);
-  fr = createP(''); // creating Paragraph element
 
   flowfield = new Array(cols*rows); //preset the size of array
-
-  // mic input
-  mic = new p5.AudioIn();
-  mic.start();
+	
   // fft = new p5.FFT();
   // fft.setInput(mic);
+
+  for (var i = 0; i < 1200; i++) {
+    particles[i] = new Particle(i);
+  }
+
+  textPg = createGraphics(width, height)
+  textPg.background(0)
+  textPg.textSize(250);
+  textPg.textWrap(WORD);
+  textPg.stroke(255)
+  textPg.fill(255)
+  textPg.text('SYNTH', 0, height/2-150, 150);
+
+  // image(textPg, 0, 0)
+
+  // save text x,y coordinates
+  for (var y = 0; y < rows+1; y++) {
+    for (var x = 0; x < cols+1; x++) {
+      var index = (x + y * cols);
+      let color = textPg.get(x*scl, y*scl)
+      if(color[0] > 205) {
+        flowfieldText[index] = true
+      } else {
+        flowfieldText[index] = false
+      }
+    }
+  }
+  
+  // check text x,y coordinates
+  for (var y = 0; y < rows+1; y++) {
+    for (var x = 0; x < cols+1; x++) {
+      var index = (x + y * cols);
+      if(flowfieldText[index] > 0) {
+        push();
+        // fill(255)
+        // translate(x*scl, y*scl);
+        // ellipse(0, 0, 10, 10)
+        pop()
+      }
+    }
+  }
+  
+  // mic input
+  // audio
+	mic = new p5.AudioIn()
+	mic.start()
   getAudioContext().resume();
 
-  for (var i = 0; i < 500; i++) {
-    particles[i] = new Particle();
-  }
-  // colorMode(RGB);
-  //background(0);
 }
 
 function draw() {
-  // background(0)
-  // reset
-  // if (frameCount % 120 == 0) {
-  // 	background(0)
-  // }
+  background(0, 20)
 
   /*
   spectrum = fft.analyze();
@@ -48,88 +83,70 @@ function draw() {
   }
   */
 
-  var yoff = 0;
   spec = mic.getLevel()*100 // 0 ~ 100
-  if(frameCount%10 === 0) console.log(spec)
 
-  for (var y = 0; y < rows; y++) {
+  var yoff = 0;
+  for (var y = 0; y < rows+1; y++) {
     var xoff = 0;
-    for (var x = 0; x < cols; x++) {
+    for (var x = 0; x < cols+1; x++) {
       var index = (x + y * cols);
-      var angle = noise(xoff, yoff, zoff) * TWO_PI + spec;
-      var v = p5.Vector.fromAngle(angle);  // "i want vector on every spot on the grid."05:10
-      v.setMag(10); // full units, no limit..need to set maximum limit
+      var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+
+      // "i want vector on every spot on the grid."05:10
+      var v = p5.Vector.fromAngle(angle);  
+      // full units, no limit..need to set maximum limit
+      v.setMag(10); 
       flowfield[index] = v;
-      xoff += inc;
 
       // draw flowfield lines
-      /* 
-      push();
-      translate(x*scl, y*scl);
-      rotate(v.heading());
-      line(0, 0, scl, 0);
-      pop();
-      */
+      // push();
+      // stroke(225)
+      // translate(x*scl, y*scl);
+      // rotate(v.heading());
+      // line(0, 0, scl, 0);
+      // pop();
+
+      xoff += inc;
     }
-    yoff += inc;
-    zoff += 0.04;  // fixed flow field, if you comment this
+    yoff += inc
+    zoff += 0.003;  // fixed flow field, if you comment this
   }
 
   for (var i = 0; i < particles.length; i++) {
-    let col = (100 + spec%160, 80, 80);
-    // console.log(col);
-    // stroke(100, col, map(sin(angle), -1, 1, 100, 255), ); //color
     
     // find appropriate, nearby vector
-    particles[i].show(col);
+    particles[i].follow(flowfield);
+    particles[i].update();
     particles[i].edges();
-    if(spec > 30) {
-      particles[i].follow(flowfield);
-      particles[i].update(col);
-    } else {
-      // particles[i].applyForce(p5.Vector(0, -10))
-      // particles[i].update(col);
-    }
-
     // particles.kill()
   }
-  fr.html(floor(frameRate()));
 }
 
 function Particle() {
 
-  this.pos = createVector(random(width), random(height));
-  this.vel = createVector(0, 0);
-  this.acc = createVector(0, 0);
+  this.pos = createVector(random(0, width), random(height/3, height/3*2-100));
+  this.vel = createVector(random(-100, 100), random(-100, 100));
+  this.acc = createVector(random(-100, 100), random(-100, 100));
   this.maxspeed = 3;
   this.lifespan = 0;
 
   this.prevPos = this.pos.copy();
 
   // update vel based on acc
-  this.update = function(col) {
+  this.update = function() {
     this.vel.add(this.acc);
     this.vel.limit(this.maxspeed);
     this.pos.add(this.vel);
     this.acc.mult(0);
-    stroke(255, 50)
-    // stroke(map(sin(this.vel.x), -1, 1,100, 255), col,  map(sin(this.vel.y), -1, 1, 200, 0), 50); //color
     this.lifespan += 1;
   }
 
-  // receive force
-  // add force to acc
-  this.applyForce = function(force) {
-    this.acc.add(force);
-  }
-  this.show = function(col) {
-    // stroke(map(sin(this.vel.x), -1, 1, 100, 255), map(sin(this.acc.y), -1, 1, 50, 200), col); //color
-    strokeWeight(spec/10);
-
-    // point(this.pos.x, this.pos.y);
+  this.show = function(col, weight) {
+    push()
+    strokeWeight(weight);
+    stroke(col)
     line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y); //draw line from current to previous pos
-
-    this.updatePrev();
+    pop()
   }
 
   this.updatePrev = function() {
@@ -139,19 +156,19 @@ function Particle() {
 
   // make sure particles come back when hit the walls
   this.edges = function() {
-    if(this.pos.x > width) {
+    if (this.pos.x > width) {
       this.pos.x = 0;
       this.updatePrev();
     }
-    if(this.pos.x < 0) {
+    if (this.pos.x < 0) {
       this.pos.x = width;
       this.updatePrev();
     }
-    if(this.pos.y > height) {
+    if (this.pos.y > height) {
       this.pos.y = 0;
       this.updatePrev();
     }
-    if(this.pos.y < 0) {
+    if (this.pos.y < 0) {
       this.pos.y = height;
       this.updatePrev();
     }
@@ -164,17 +181,34 @@ function Particle() {
     var y = floor(this.pos.y / scl);
     var index = x + y * cols;
     var force = vectors[index];
+
+    if(flowfieldText[index]) this.show(color('#80A1D4'), 2)
+    else this.show(color('#F7F4EA'), 0.1)
     this.applyForce(force);
+    this.updatePrev();
   }
 
+  // add force to acc
+  this.applyForce = function(force) {
+    this.acc.add(force);
+  }
+  
   this.isDead = () => {
     if(this.lifespan >= 255) return true 
     else return false
   }
 
   this.drop = function() {
-    let gravity = createVector(0, -5)
+    let gravity = createVector(0, 100)
     this.applyForce(gravity)
   }
 
 }
+
+
+function mousePressed() {
+  if (getAudioContext().state !== 'running') {
+    getAudioContext().resume();
+  }
+}
+
